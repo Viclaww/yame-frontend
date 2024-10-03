e085f273b613cac598b3010492e2a1a73d8d95ff
Victor Lawrence <viclawrence99@gmail.com>
2024-10-03 21:26:58 +0100

    the

--- app/components/layout.tsx
+++ app/components/layout.tsx
@@ -1,35 +1,56 @@
 import { ReactNode } from "react";
 import DasHead from "./DasHead";
-import { RiHome7Fill } from "react-icons/ri";
-import { BsPeople } from "react-icons/bs";
+import { RiHome7Fill, RiHome7Line } from "react-icons/ri";
+import { BsPeople, BsPeopleFill } from "react-icons/bs";
 import { FaPenNib } from "react-icons/fa";
-import { IoMdNotifications } from "react-icons/io";
+import { IoMdNotifications, IoMdNotificationsOutline } from "react-icons/io";
 import { Link, useLocation } from "@remix-run/react";
+import { PiPenNibLight } from "react-icons/pi";
 
 const Layout = ({ children }: { children: ReactNode }) => {
   const pathname = useLocation().pathname;
   const navItems = [
     {
       id: 1,
-      icon: (color?: string) => <RiHome7Fill size={25} fill={color} />,
+      icon: (active: boolean, color?: string) =>
+        active ? (
+          <RiHome7Fill size={25} fill={color} />
+        ) : (
+          <RiHome7Line size={25} color={color} fill={color} />
+        ),
       name: "Home",
       link: "/",
     },
     {
       id: 2,
-      icon: (color?: string) => <BsPeople size={25} fill={color} />,
+      icon: (active: boolean, color?: string) =>
+        active ? (
+          <BsPeopleFill size={25} fill={color} />
+        ) : (
+          <BsPeople size={25} color={color} fill={color} />
+        ),
       name: "People",
       link: "/people",
     },
     {
       id: 3,
-      icon: (color?: string) => <FaPenNib size={25} fill={color} />,
+      icon: (active: boolean, color?: string) =>
+        active ? (
+          <FaPenNib size={25} color="" fill={color} />
+        ) : (
+          <PiPenNibLight size={25} color={color} fill={color} />
+        ),
       name: "Write",
       link: "/write",
     },
     {
       id: 4,
-      icon: (color?: string) => <IoMdNotifications size={25} fill={color} />,
+      icon: (active: boolean, color?: string) =>
+        active ? (
+          <IoMdNotifications size={25} fill={color} />
+        ) : (
+          <IoMdNotificationsOutline size={25} color={color} fill={color} />
+        ),
       name: "Notifications",
       link: "/notifications",
     },
@@ -42,11 +63,13 @@ const Layout = ({ children }: { children: ReactNode }) => {
           <Link
             key={item.id}
             to={item.link}
-            className={`flex items-center gap-2 p-3 hover:bg-[#292727] rounded-full ${
+            className={`flex items-center gap-2 p-3 text-white  rounded-full ${
               pathname.includes(item.link) ? "bg-[#A89BB8]" : ""
             }`}
           >
-            {pathname.includes(item.link) ? item.icon("#7851A9") : item.icon()}
+            {pathname.includes(item.link)
+              ? item.icon(pathname.includes(item.link), "#7851A9")
+              : item.icon(pathname.includes(item.link), "fff")}
           </Link>
         ))}
         <Link
