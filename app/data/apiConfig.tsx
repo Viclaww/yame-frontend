82294bc2bd4cafa70cf8572c5ae41b41ec34d39f
yusuf-ishaku <kingshakzy@gmail.com>
2024-10-08 23:33:51 +0100

    Clean up

--- app/data/apiConfig.tsx
+++ app/data/apiConfig.tsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const baseConfig = axios.create({
-  baseURL: "https://hackathon-pr.onrender.com/", // Replace with your API base URL
+  baseURL: "http://localhost:3000/", // Replace with your API base URL
   timeout: 10000, // Request timeout in milliseconds
   headers: {
     "Content-Type": "application/json",
