382f85bffea3d8ea5def18a104aee609c1332173
yusuf-ishaku <kingshakzy@gmail.com>
2024-10-11 02:16:23 +0100

    Clean up

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,18 +1,26 @@
 import type { MetaFunction } from "@remix-run/node";
+import { useEffect } from "react";
+import { useNavigate } from "@remix-run/react";
 import Footer from "~/components/Footer";
 import FAQSection from "~/components/landing/FAQs";
 import Hero from "~/components/landing/Hero";
 import HowItWorks from "~/components/landing/HowItWorks";
 import WhyChooseUs from "~/components/landing/WhyChooseUs";
-
 export const meta: MetaFunction = () => {
   return [
     { title: "Huddle" },
     { name: "description", content: "Welcome to Huddle, AI Powered Study Collaboration tool" },
+    
   ];
 };
-
 export default function Index() {
+  const navigate = useNavigate();
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      navigate("/home");
+    }, 2000);
+    return () => clearTimeout(timer);
+  }, [navigate]);
   return (
     <div className="flex bg-[#070707] flex-col items-center justify-center">
       <Hero />
