3dc95d71e8fa16e1baa9b45427265e83d5c21012
Victor Lawrence <viclawrence99@gmail.com>
2024-10-12 08:37:32 +0100

    footer

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -1,5 +1,5 @@
 const Footer = () => {
-  return <div className="w-full text-center py-2">© Powered by AppLift</div>;
+  return <div className="w-full text-center py-2">© Powered by GDG on Campus Hackathon</div>;
 };
 
 export default Footer;
