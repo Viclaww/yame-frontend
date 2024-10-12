bd5ccda91a0a52372779e783a804dbda1c465c3e
yusuf-ishaku <kingshakzy@gmail.com>
2024-10-12 09:40:17 +0100

    Clean up

--- app/components/DasHead.tsx
+++ app/components/DasHead.tsx
@@ -1,17 +1,18 @@
 import { Link } from "@remix-run/react";
-
+import {CgMenu} from "react-icons/cg";
+import { useState } from "react";
 export default function DasHead({ isUser }: { isUser: boolean }) {
-  // const [isOpened, setIsOpened] = useState(false);
+  const [isOpened, setIsOpened] = useState(false);
   return (
     <div className="flex flex-row justify-between relative z-30 p-2 md:p-6 py-4  w-full">
       <h2 className="text-lg md:text-3xl cursor-pointer text-yame-purple dark:text-white">huddle.</h2>
-      {/* <button
+      <button
         onClick={() => setIsOpened(!isOpened)}
         className="md:hidden"
         aria-label="Toggle menu"
       >
         <CgMenu size={30} />
-      </button> */}
+      </button>
       {isUser ? (
         <div className="flex gap-2 cursor-pointer">
           <div>
@@ -26,14 +27,13 @@ export default function DasHead({ isUser }: { isUser: boolean }) {
             to="/test"
             className="bg-yame-purple text-white cursor-pointer py-1 flex justify-center w-fit px-4 md:w-[160px] rounded-lg"
           >
-            Take Test
+            Take Quiz
           </Link>
         </div>
       ) : (
         <div
-          className={`flex gap-2 flex-col md:flex-row md: duration-300 items-start md:bg-transparent bg-slate-600 absolute h-screen top-0 left-0 pt-10 pl-3 md:p-0 md:w-auto md:h-auto md:relative w-2/3 cursor-pointer ${
-            //  isOpened ? "translate-x-0" :
-            "-translate-x-[150%] md:translate-x-0"
+          className={`flex gap-2 flex-col md:flex-row duration-300 items-start md:bg-transparent bg-slate-600 absolute h-screen top-0 left-0 pt-10 pl-3 md:p-0 md:w-auto md:h-auto md:relative w-2/3 cursor-pointer ${
+             isOpened ? "translate-x-0" : "-translate-x-[150%] md:translate-x-0"
           }`}
         >
           <Link
